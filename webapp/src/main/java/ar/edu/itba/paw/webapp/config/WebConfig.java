895beac2f1cb7e96538d41deb12f81df6de1c28d
Francisco <frbernad@itba.edu.ar>
2021-03-15 21:20:41 -0300

    build: fix father pom and add jsp structure to WEB-INF

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,11 +1,27 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @ComponentScan({"ar.edu.itba.paw.webapp.controller",})
 @Configuration
 public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setPrefix(".jsp");
+
+        return resolver;
+    }
+
 }
