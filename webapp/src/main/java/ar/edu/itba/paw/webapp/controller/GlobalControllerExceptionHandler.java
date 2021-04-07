955cbaf5781692682d8279e67c446c4cbb058207
Francisco <frbernad@itba.edu.ar>
2021-04-07 18:53:59 -0300

    fix: reorder controllers and not found

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GlobalControllerExceptionHandler.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GlobalControllerExceptionHandler.java
@@ -32,10 +32,8 @@ public class GlobalControllerExceptionHandler {
         String error = messageSource.getMessage("errors.NotFound.Job", null, locale);
         String code = HttpStatus.NOT_FOUND.toString();
         final ModelAndView mav = new ModelAndView(NOT_FOUND_VIEW);
-        System.out.println(code);
-        mav.addObject("errors",error);
-        mav.addObject("code",code);
-
+        mav.addObject("errors", error);
+        mav.addObject("code", code);
         return mav;
     }
 
@@ -46,50 +44,50 @@ public class GlobalControllerExceptionHandler {
         String error = messageSource.getMessage("errors.NotFound.User", null, locale);
         String code = HttpStatus.NOT_FOUND.toString();
         final ModelAndView mav = new ModelAndView(NOT_FOUND_VIEW);
-        mav.addObject("errors",error);
-        mav.addObject("code",code);
+        mav.addObject("errors", error);
+        mav.addObject("code", code);
         return mav;
     }
 
 
     /*By default when the DispatcherServlet can't find a handler for a request it sends a 404 response. However if its property "throwExceptionIfNoHandlerFound" is set to true this exception is raised and may be handled with a configured HandlerExceptionResolver.
-    * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/NoHandlerFoundException.html
-    * https://stackoverflow.com/questions/13356549/handle-error-404-with-spring-controller/46704230
-    * */
+     * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/NoHandlerFoundException.html
+     * https://stackoverflow.com/questions/13356549/handle-error-404-with-spring-controller/46704230
+     * */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ModelAndView resourceNotFoundException(){
+    public ModelAndView resourceNotFoundException() {
         Locale locale = LocaleContextHolder.getLocale();
-        String error = messageSource.getMessage("errors.NotFound", null, locale);
+        String error = messageSource.getMessage("errors.NotFound.Resource", null, locale);
         String code = HttpStatus.NOT_FOUND.toString();
         final ModelAndView mav = new ModelAndView(NOT_FOUND_VIEW);
-        mav.addObject("errors",error);
-        mav.addObject("code",code);
+        mav.addObject("errors", error);
+        mav.addObject("code", code);
         return mav;
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(TypeMismatchException.class)
-    public ModelAndView badRequestException(){
+    public ModelAndView badRequestException() {
         Locale locale = LocaleContextHolder.getLocale();
         String error = messageSource.getMessage("errors.BadRequest", null, locale);
         String code = HttpStatus.BAD_REQUEST.toString();
         final ModelAndView mav = new ModelAndView(NOT_FOUND_VIEW);
-        mav.addObject("errors",error);
-        mav.addObject("code",code);
+        mav.addObject("errors", error);
+        mav.addObject("code", code);
         return mav;
     }
 
     /*Server error */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ModelAndView serverException(){
+    public ModelAndView serverException() {
         Locale locale = LocaleContextHolder.getLocale();
         String error = messageSource.getMessage("errors.ServerError", null, locale);
         String code = HttpStatus.INTERNAL_SERVER_ERROR.toString();
         final ModelAndView mav = new ModelAndView(ERROR_VIEW);
-        mav.addObject("errors",error);
-        mav.addObject("code",code);
+        mav.addObject("errors", error);
+        mav.addObject("code", code);
         return mav;
     }
 
