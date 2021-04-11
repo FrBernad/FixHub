21f0246f03b738ff396f766c4261908c0823ae85
Francisco <frbernad@itba.edu.ar>
2021-04-11 12:28:15 -0300

    feat: add simple user interface and logging

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/UserDetailService.java
@@ -0,0 +1,37 @@
+package ar.edu.itba.paw.webapp.auth;
+
+import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+@Component
+public class UserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        final Optional<User> user = userService.getUserByEmail(username);
+
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("No user by the name" + username);
+        }
+
+        final Collection<? extends GrantedAuthority> authorities = Arrays.asList(
+            new SimpleGrantedAuthority("ROLE_USER"),
+            new SimpleGrantedAuthority("ROLE_ADMIN")
+        );
+        return new org.springframework.security.core.userdetails.User(username, user.get().getPassword(), authorities);
+    }
+}
