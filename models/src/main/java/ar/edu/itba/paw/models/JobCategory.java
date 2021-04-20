23af9ec835bb08a043c9f1672ddf61086348e4ea
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-04-20 14:58:03 -0300

    feat: add new Jobs

--- models/src/main/java/ar/edu/itba/paw/models/JobCategory.java
+++ models/src/main/java/ar/edu/itba/paw/models/JobCategory.java
@@ -11,7 +11,19 @@ public enum JobCategory {
     CARPINTERO,
     PINTOR,
     HERRERO,
-    TECHISTA;
+    TECHISTA,
+    LIMPIEZA,
+    MANTENIMIENTO,
+    ENTREGA,
+    MUDANZA,
+    NINERA,
+    PASEADOR_DE_PERRO,
+    CUIDADOR_DE_ANCIANO,
+    CHEF,
+    CATERING,
+    VIDRIERO,
+    FUMIGADOR,
+    FOTOGRAFO;
 
    public static boolean contains(String value){
         return Arrays.stream(values()).map(Enum::name).anyMatch(code -> code.equals(value));
