48ce78b86394c07fb18ec434aceb7f982e49f3d6
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-04-21 17:02:54 -0300

    refactor: change test table

--- persistence/src/test/resources/user-test.sql
+++ persistence/src/test/resources/user-test.sql
@@ -1,3 +1,3 @@
 TRUNCATE TABLE users RESTART IDENTITY AND COMMIT NO CHECK;
-insert into USERS(password, name, surname, email, phone_number, state, city)
-VALUES ('password','name','surname','email','phoneNumber','state','city');
+insert into USERS(u_password, u_name, u_surname, u_email, u_phone_number, u_profile_picture, u_cover_picture, u_state, u_city)
+VALUES ('password','name','surname','email','phoneNumber',null,null,'state','city');
