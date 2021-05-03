40a4b487f8be150050e88a7f0c969449cb9415fb
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-05-03 17:48:35 -0300

    fix: PasswordResetTokenDaoTest

--- persistence/src/test/java/ar/edu/itba/paw/persistence/PasswordResetTokenDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/PasswordResetTokenDaoTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,23 +13,16 @@ import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 @Transactional
-@Sql(scripts = "classpath:review-dao-test.sql")
+@Sql(scripts = "classpath:password-reset-token-dao-test.sql")
 public class PasswordResetTokenDaoTest {
 
     private static final long ID = 1L;
-    private static final String TOKEN = UUID.randomUUID().toString();
-    private static final LocalDateTime EXPIRATION_DATE = LocalDateTime.now().plusHours(1);
 
     @Autowired
     private PasswordResetTokenDao passwordResetTokenDao;
@@ -41,26 +32,16 @@ public class PasswordResetTokenDaoTest {
 
     private JdbcTemplate jdbcTemplate;
 
-    private SimpleJdbcInsert jdbcInsert;
 
     @Before
     public void setUp() {
         this.jdbcTemplate = new JdbcTemplate(ds);
-        this.jdbcInsert = new SimpleJdbcInsert(ds).withTableName("password_reset_tokens").usingGeneratedKeyColumns("prt_id");
     }
 
     @Test
     public void testRemoveTokenByUserId() {
 
-        Map<String, Object> values = new HashMap<>();
-        values.put("prt_user_id", ID);
-        values.put("prt_token", TOKEN);
-        values.put("prt_expiration_date", Timestamp.valueOf(EXPIRATION_DATE));
-
-        jdbcInsert.execute(values);
-
         passwordResetTokenDao.removeTokenByUserId(ID);
-
         assertEquals(0, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "password_reset_tokens", "prt_id = " + ID));
 
     }
---
+++ persistence/src/test/resources/password-reset-token-dao-test.sql
@@ -0,0 +1,8 @@
+TRUNCATE TABLE users RESTART IDENTITY AND COMMIT NO CHECK;
+TRUNCATE TABLE password_reset_tokens RESTART IDENTITY AND COMMIT NO CHECK;
+
+insert into USERS(u_id, u_password, u_name, u_surname, u_email, u_phone_number,
+                  u_profile_picture, u_cover_picture, u_state, u_city)
+VALUES (1,'password', 'Ignacio', 'Lopez', 'ignacio@yopmail.com', '5491112345678', null, null, 'Caballito', 'CABA');
+
+INSERT INTO PASSWORD_RESET_TOKENS(prt_user_id, prt_token, prt_expiration_date) VALUES(1, '83feb0af-f467-4374-91fb-8f96db3f9a23', '2021-05-03 21:45:46.662');
