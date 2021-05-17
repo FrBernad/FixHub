fd7ea5901e810a41f4df5ceede8807f2422ec1dd
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-05-17 10:13:02 -0300

    feat:  add logs in locationDao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/LocationDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/LocationDaoImpl.java
@@ -24,18 +24,25 @@ public class LocationDaoImpl implements LocationDao {
     @PersistenceContext
     private EntityManager em;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationDaoImpl.class);
+
+
     @Override
     public Collection<State> getStates() {
+
+        LOGGER.debug("Getting all states");
         return em.createQuery("FROM State", State.class).getResultList();
     }
 
     @Override
     public Optional<State> getStateById(long stateId) {
+        LOGGER.debug("Getting state with id {}", stateId);
         return Optional.ofNullable(em.find(State.class, stateId));
     }
 
     @Override
     public Collection<City> getCitiesByState(State state) {
+        LOGGER.debug("Getting cities of the state  {}", state.getName());
         return em
             .createQuery("FROM City WHERE state = :state", City.class)
             .setParameter("state", state)
@@ -44,6 +51,8 @@ public class LocationDaoImpl implements LocationDao {
 
     @Override
     public Optional<City> getCityByCityAndStateId(long cityId, State state) {
+        LOGGER.debug("Getting city with id {} of the state  {}", cityId,state.getName());
+
         return em
             .createQuery("FROM City WHERE id = :cityId AND state = :state", City.class)
             .setParameter("state", state)
@@ -52,59 +61,4 @@ public class LocationDaoImpl implements LocationDao {
             .stream()
             .findFirst();
     }
-
-//    private final JdbcTemplate jdbcTemplate;
-//
-//    private static final ResultSetExtractor<Collection<State>> STATE_ROW_MAPPER = rs -> {
-//        List<State> states = new LinkedList<>();
-//        while (rs.next()) {
-//            states.add(new State(rs.getLong("s_id"), rs.getString("s_name")));
-//        }
-//        return states;
-//    };
-//
-//    private static final ResultSetExtractor<Collection<City>> CITY_ROW_MAPPER = rs -> {
-//        List<City> cities = new LinkedList<>();
-//        while (rs.next()) {
-//            cities.add(new City(rs.getLong("c_id"), rs.getString("c_name")));
-//        }
-//        return cities;
-//    };
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(LocationDaoImpl.class);
-//
-//
-//    @Autowired
-//    public LocationDaoImpl(final DataSource ds) {
-//        jdbcTemplate = new JdbcTemplate(ds);
-//    }
-//
-//
-//    @Override
-//    public Collection<State> getStates() {
-//        final String query = "SELECT * FROM STATES";
-//        LOGGER.debug("Executing query: {}", query);
-//        return jdbcTemplate.query(query, STATE_ROW_MAPPER);
-//    }
-//
-//    @Override
-//    public Optional<State> getStateById(long stateId) {
-//        final String query = "SELECT * FROM STATES WHERE s_id = ?";
-//        LOGGER.debug("Executing query: {}", query);
-//        return jdbcTemplate.query(query, new Object[]{stateId}, STATE_ROW_MAPPER).stream().findFirst();
-//    }
-//
-//    @Override
-//    public Collection<City> getCitiesByStateId(long stateId) {
-//        final String query = "SELECT * FROM CITIES WHERE c_state_id = ?";
-//        LOGGER.debug("Executing query: {}", query);
-//        return jdbcTemplate.query(query, new Object[]{stateId}, CITY_ROW_MAPPER);
-//    }
-//
-//    @Override
-//    public Optional<City> getCityByCityAndStateId(long cityId, long stateId) {
-//        final String query = "SELECT * FROM CITIES WHERE c_id = ? AND c_state_id = ?";
-//        LOGGER.debug("Executing query: {}", query);
-//        return jdbcTemplate.query(query, new Object[]{cityId, stateId}, CITY_ROW_MAPPER).stream().findFirst();
-//    }
 }
