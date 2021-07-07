aa71a017ba335862c1f55d1a92900249ea49ff43
Francisco <frbernad@itba.edu.ar>
2021-07-07 12:34:17 -0300

    fix: login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,17 @@
 import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
-import {faPlus as fasPlus, faStar as fasStar, faSearch as fasSearch, faHandshake as fasHandshake} from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus as fasPlus,
+  faStar as fasStar,
+  faSearch as fasSearch,
+  faHandshake as fasHandshake,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faEyeSlash as farEyeSlash,
+  faEye as farEye
+} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +25,7 @@ export class AppComponent {
     translateService.addLangs(['en', 'es']);
     translateService.setDefaultLang('en');
 
-    library.addIcons(fasPlus, fasSearch, fasStar, fasHandshake);
+    library.addIcons(fasPlus, fasSearch, fasStar, fasHandshake, farEyeSlash, farEye);
   }
 
   translateSite(language: string) {
--- webapp/src/main/webapp/WEB-INF/jsp/views/login.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/views/login.jsp
@@ -2,7 +2,9 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
 <html>
+
 <head>
     <title><spring:message code="productName"/> | <spring:message code="login.title"/></title>
 
