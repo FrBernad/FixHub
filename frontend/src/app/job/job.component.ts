9af51a520f71a130f85d803328bdb2949157739d
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-07-07 19:01:50 -0300

    feat: add review form modal

--- frontend/src/app/job/job.component.ts
+++ frontend/src/app/job/job.component.ts
@@ -8,22 +8,23 @@ import {User} from "../models/User";
   styleUrls: ['./job.component.scss']
 })
 export class JobComponent implements OnInit {
+
   provider: User = {
-    id:2,
-    name:'Agus',
-    surname:'Manfredi',
-    email:'agus@yopmail.com',
-    phoneNumber:'+5491112345678',
-    state:'Buenos Aires',
-    city:'Adrogue',
+    id: 2,
+    name: 'Agus',
+    surname: 'Manfredi',
+    email: 'agus@yopmail.com',
+    phoneNumber: '+5491112345678',
+    state: 'Buenos Aires',
+    city: 'Adrogue',
     profileImage: 1,
     coverImage: 2,
-    following: [] ,
+    following: [],
     followers: [],
     providerDetails: {
       location: {
-        cities: [{id:1,name:'Burzaco'}],
-        state:  {id:1,name:'Buenos Aires'}
+        cities: [{id: 1, name: 'Burzaco'}],
+        state: {id: 1, name: 'Buenos Aires'}
       },
       schedule: {
         startTime: new Date(),
@@ -33,11 +34,12 @@ export class JobComponent implements OnInit {
   }
 
   job: Job = {
-    id: 1, description: 'asda',jobProvided: 'dasda',
-    category:'CARPINTERO',price:121,totalRatings:0,
-    averageRating:0,images:[],reviews:[],
+    id: 1, description: 'asda', jobProvided: 'dasda',
+    category: 'CARPINTERO', price: 121, totalRatings: 0,
+    averageRating: 0, images: [], reviews: [],
     provider: this.provider,
-    paused:false} ;
+    paused: false
+  };
   loggedUser: User;
   canReview: boolean;
 
@@ -51,14 +53,14 @@ export class JobComponent implements OnInit {
   constructor() {
   }
 
-  getStartTime(){
+  getStartTime() {
     let startTime = this.job.provider.providerDetails.schedule.startTime;
-    return startTime.getHours()+':'+startTime.getMinutes();
+    return startTime.getHours() + ':' + startTime.getMinutes();
   }
 
-  getEndTime(){
+  getEndTime() {
     let endTime = this.job.provider.providerDetails.schedule.endTime;
-    return endTime.getHours()+':'+endTime.getMinutes();
+    return endTime.getHours() + ':' + endTime.getMinutes();
   }
 
   ngOnInit(): void {
--- frontend/src/app/job/review-form/review-form.component.ts
+++ frontend/src/app/job/review-form/review-form.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {min} from "rxjs/operators";
 
 @Component({
   selector: 'app-review-form',
@@ -9,27 +8,27 @@ import {min} from "rxjs/operators";
 })
 export class ReviewFormComponent implements OnInit {
 
-  rates : number[] = [1,2,3,4,5]
+  rates: number[] = [1, 2, 3, 4, 5]
   reviewForm: FormGroup;
-  minDescLength:number = 4;
-  maxDescLength:number = 300;
-  minRate:number=1;
-  maxRate:number=5;
+  minDescLength: number = 4;
+  maxDescLength: number = 300;
+  minRate: number = 1;
+  maxRate: number = 5;
 
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     //key value pairs
     this.reviewForm = new FormGroup({
-      'description': new FormControl(null,[Validators.required,Validators.minLength(this.minDescLength),Validators.maxLength(this.maxDescLength)]),//initial values, validators,async validators
-      'rating': new FormControl("1",[Validators.required,Validators.min(this.minRate),Validators.max(this.maxRate)])
+      'description': new FormControl(null, [Validators.required, Validators.minLength(this.minDescLength), Validators.maxLength(this.maxDescLength)]),//initial values, validators,async validators
+      'rating': new FormControl("1", [Validators.required, Validators.min(this.minRate), Validators.max(this.maxRate)])
     })
 
   }
 
-  onSubmit(){
-    console.log(this.reviewForm);
+  onSubmit() {
 
   }
 
