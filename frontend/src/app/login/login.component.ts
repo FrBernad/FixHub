25bb7b1617ea38a921ca51b9479f077c0781b4e7
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-07-07 22:58:15 -0300

    feat: add loginForm

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import {TranslateService} from "@ngx-translate/core";
 import {Subscription} from "rxjs";
 import {AppComponent} from "../app.component";
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -14,11 +15,24 @@ export class LoginComponent implements OnInit {
 
   showPass = true;
   recoverPass = false;
+  loginForm: FormGroup;
+  error=false;
+
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.loginForm= new FormGroup({
+      'email': new FormControl(null, [Validators.required]),
+      'password': new FormControl(null, [Validators.required]),
+      'rememberMe': new FormControl(false)
+    });
+
+  }
+
+  onSubmit() {
+
   }
 
   toogleEye() {
