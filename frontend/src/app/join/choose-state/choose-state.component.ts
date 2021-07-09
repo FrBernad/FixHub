802088703fe6e116a79d293367f574997329103d
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-07-09 00:25:39 -0300

    feat: add time picker and validators in join forms

--- frontend/src/app/join/choose-state/choose-state.component.ts
+++ frontend/src/app/join/choose-state/choose-state.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-choose-state',
@@ -9,18 +9,30 @@ import {FormGroup} from "@angular/forms";
 export class ChooseStateComponent implements OnInit {
 
   chooseStateForm: FormGroup;
-  states = [];
+  stateSelected = '';
+  @Output() stateChosen = new EventEmitter<void>();
 
+  states = [{id:1,name:'Buenos Aires'},{id:2,name:'CABA'}];
   constructor() { }
 
   ngOnInit(): void {
     this.chooseStateForm = new FormGroup({
-
+      'startTime' : new FormControl(null,[Validators.required,Validators.pattern('([0-9]|[10-12]):([0-5][0-9]) (AM|PM)')]),
+      'endTime' : new FormControl(null,[Validators.required,Validators.pattern('([0-9]|[10-12]):([0-5][0-9]) (AM|PM)')]),
+      'state' : new FormControl(null,[Validators.required])
     });
   }
 
   onSubmit(){
-
+    if(!this.chooseStateForm.valid){
+      this.chooseStateForm.markAllAsTouched();
+      return;
+    }
+    this.stateChosen.emit();
   }
 
+  selectState(state:{id:number, name:string}){
+    this.stateSelected = state.name;
+    this.chooseStateForm.patchValue({'state': state});
+  }
 }
