94bf4df05c26f88ea3f170e5d40b26e4828adad1
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-07-11 13:44:47 -0300

    fix: confirm password validation

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,17 +1,16 @@
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {UserService} from "../auth/user.service";
-import {AuthService} from "../auth/auth.service";
-import {PreviousRouteService} from "../auth/previous-route.service";
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../auth/user.service';
+import { AuthService } from '../auth/auth.service';
+import { PreviousRouteService } from '../auth/previous-route.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   showPass1 = true;
   showPass2 = true;
 
@@ -31,19 +30,72 @@ export class RegisterComponent implements OnInit {
     private userService: UserService,
     private previousRouteService: PreviousRouteService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
-      name: new FormControl(null, [Validators.required, Validators.maxLength(this.maxNameLength), Validators.pattern("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]*$")]),
-      surname: new FormControl(null, [Validators.required, Validators.maxLength(this.maxSurnameLength), Validators.pattern("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]*$")]),
-      email: new FormControl(null, [Validators.required, Validators.maxLength(this.maxEmailLength), Validators.pattern("^([a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+)*$")]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(this.minPasswordLength), Validators.maxLength(this.maxPasswordLength)]),
-      phoneNumber: new FormControl(null, [Validators.required, Validators.maxLength(this.maxPhoneNumberLength), Validators.pattern("^[+]?(?:(?:00)?549?)?0?(?:11|15|[2368]\\d)(?:(?=\\d{0,2}15)\\d{2})??\\d{8}$")]),
-      state: new FormControl(null, [Validators.required, Validators.maxLength(this.maxStateLength), Validators.pattern("^[0-9a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]*$")]),
-      city: new FormControl(null, [Validators.required, Validators.maxLength(this.maxCityLength), Validators.pattern("^[0-9a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]*$")]),
-    })
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(this.maxNameLength),
+        Validators.pattern(
+          "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]*$"
+        ),
+      ]),
+      surname: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(this.maxSurnameLength),
+        Validators.pattern(
+          "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]*$"
+        ),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(this.maxEmailLength),
+        Validators.pattern('^([a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+)*$'),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(this.minPasswordLength),
+        Validators.maxLength(this.maxPasswordLength),
+      ]),
+      confirmPassword: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(this.minPasswordLength),
+        Validators.maxLength(this.maxPasswordLength),
+      ]),
+      phoneNumber: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(this.maxPhoneNumberLength),
+        Validators.pattern(
+          '^[+]?(?:(?:00)?549?)?0?(?:11|15|[2368]\\d)(?:(?=\\d{0,2}15)\\d{2})??\\d{8}$'
+        ),
+      ]),
+      state: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(this.maxStateLength),
+        Validators.pattern(
+          "^[0-9a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]*$"
+        ),
+      ]),
+      city: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(this.maxCityLength),
+        Validators.pattern(
+          "^[0-9a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]*$"
+        ),
+      ]),
+    });
+    this.registerForm.setValidators(this.passwordMatching.bind(this));
+  }
+
+  passwordMatching(group: FormGroup): { [s: string]: boolean } {
+    const confirmPasswordControl = group.controls['confirmPassword'];
+    if(group.controls['password'].value != confirmPasswordControl.value){
+      confirmPasswordControl.setErrors({ passwordsDontMatch: true });
+    }else {
+      confirmPasswordControl.setErrors(null);
+    }
+    return;
   }
 
   toogleEye1() {
@@ -55,7 +107,7 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("hola")
+    console.log(this.registerForm);
 
     if (!this.registerForm.valid) {
       this.registerForm.markAllAsTouched();
@@ -69,38 +121,38 @@ export class RegisterComponent implements OnInit {
       password: this.registerForm.value.password,
       phoneNumber: this.registerForm.value.phoneNumber,
       state: this.registerForm.value.state,
-      city: this.registerForm.value.city
-    }
+      city: this.registerForm.value.city,
+    };
 
-    console.log(registerData)
+    console.log(registerData);
 
     const authObs = this.authService.signup(registerData);
 
     authObs.subscribe(
       () => {
-        console.log("registered!")
-        this.authService.login(registerData.email, registerData.password)
-          .subscribe(() => {
-              this.userService
-                .populateUserData()
-                .subscribe(() => {
-                  let url = '/user/profile';
-                  if (this.previousRouteService.getAuthRedirect()) {
-                    let prevUrl = this.previousRouteService.getPreviousUrl();
-                    url = !!prevUrl ? prevUrl : url;
-                    this.previousRouteService.setAuthRedirect(false);
-                  }
-                  this.router.navigate([url]);
-                });
+        console.log('registered!');
+        this.authService
+          .login(registerData.email, registerData.password)
+          .subscribe(
+            () => {
+              this.userService.populateUserData().subscribe(() => {
+                let url = '/user/profile';
+                if (this.previousRouteService.getAuthRedirect()) {
+                  let prevUrl = this.previousRouteService.getPreviousUrl();
+                  url = !!prevUrl ? prevUrl : url;
+                  this.previousRouteService.setAuthRedirect(false);
+                }
+                this.router.navigate([url]);
+              });
             },
-            errorMessage => {
+            (errorMessage) => {
               console.log(errorMessage);
             }
           );
       },
-      errorMessage => {
+      (errorMessage) => {
         console.log(errorMessage);
-      })
+      }
+    );
   }
 }
-
