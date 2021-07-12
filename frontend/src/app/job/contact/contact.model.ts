74233a820d5dd9690282ed944d838d094eb28143
Joaco Lega <jlegammare@itba.edu.ar>
2021-07-12 10:48:28 -0300

    fix: contact model refactor

--- frontend/src/app/job/contact/contact.model.ts
+++ frontend/src/app/job/contact/contact.model.ts
@@ -1,35 +1,69 @@
 import {JobStatusModel} from "../../models/job-status.model";
+import {User} from "../../models/user.model";
+import {Job} from "../../models/job.model";
+import {ContactInfo} from "../../models/contactInfo.model";
 
 export class Contact {
-  public state: string;
-  public city: string;
-  public street: string;
-  public addressNumber: number;
-  public floor: number;
-  public departmentNumber: string;
-  public message: string;
-  public date: Date;
-  public status: JobStatusModel
 
-  constructor(state: string, city: string, street: string, addressNumber: number, floor: number, departmentNumber: string, message: string, date:Date) {
-    this.state = state;
-    this.city = city;
-    this.street = street;
-    this.addressNumber = addressNumber;
-    this.floor = floor;
-    this.departmentNumber = departmentNumber;
-    this.message = message;
-    this.date= date;
-    this.status=JobStatusModel.PENDING;
+  private _id:number;
+  private _user:User;
+  private _provider:User;
+  private _message: string;
+  private _status: JobStatusModel;
+  private _date: Date;
+  private _job:Job;
+  private _contactInfo:ContactInfo;
+
+
+  constructor(id: number, user: User, provider: User, message: string, status: JobStatusModel, date: Date, job: Job, contactInfo: ContactInfo) {
+    this._id = id;
+    this._user = user;
+    this._provider = provider;
+    this._message = message;
+    this._status = status;
+    this._date = date;
+    this._job = job;
+    this._contactInfo = contactInfo;
   }
 
   isWorkInProgress():boolean{
-    return this.status==JobStatusModel.IN_PROGRESS;
+    return this._status==JobStatusModel.IN_PROGRESS;
   }
 
   isWorkPending():boolean{
-    return this.status==JobStatusModel.PENDING;
+    return this._status==JobStatusModel.PENDING;
   }
 
 
+  get id(): number {
+    return this._id;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get provider(): User {
+    return this._provider;
+  }
+
+  get message(): string {
+    return this._message;
+  }
+
+  get status(): JobStatusModel {
+    return this._status;
+  }
+
+  get date(): Date {
+    return this._date;
+  }
+
+  get job(): Job {
+    return this._job;
+  }
+
+  get contactInfo(): ContactInfo {
+    return this._contactInfo;
+  }
 }
--- frontend/src/app/models/contactInfo.model.ts
+++ frontend/src/app/models/contactInfo.model.ts
@@ -6,4 +6,14 @@ export class ContactInfo {
   floor: string;
   state: string;
   street: string;
+
+  constructor(id: number, addressNumber: string, city: string, departmentNumber: string, floor: string, state: string, street: string) {
+    this.id = id;
+    this.addressNumber = addressNumber;
+    this.city = city;
+    this.departmentNumber = departmentNumber;
+    this.floor = floor;
+    this.state = state;
+    this.street = street;
+  }
 }
