cfbe40deff81f5104904c4db944f759987199a58
Joaco Lega <jlegammare@itba.edu.ar>
2021-07-12 23:41:05 -0300

    fix: dashboard bugs

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,9 @@ import {JobCategoryModel} from "../models/jobCategory.model";
 import {UserService} from "../auth/user.service";
 import {Contact} from "../job/contact/contact.model";
 import {Subscription} from "rxjs";
+import {JobStatusModel} from "../models/job-status.model";
+import {ContactInfo} from "../models/contactInfo.model";
+import {ContactService} from "../job/contact/contact.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -14,62 +17,6 @@ import {Subscription} from "rxjs";
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-
-  providerDetails: ProviderDetails = {
-    location: {
-      cities: [{id: 1, name: 'Burzaco'}, {id: 2, name: 'Beriso'}],
-      state: {id: 1, name: 'Buenos Aires'}
-    },
-    schedule: {
-      startTime: new Date(),
-      endTime: new Date()
-    },
-    jobsCount: 20,
-    avgRating: 4,
-    reviewCount: 35
-  };
-
-  provider = new User(
-    1,
-    'Agus',
-    'Manfredi',
-    'agus@yopmail.com',
-    '+5491112345678',
-    'Buenos Aires',
-    'Adrogue',
-    '',
-    '',
-    ['PROVIDER', 'VERIFIED'], 1, 2, this.providerDetails);
-
-  job: Job = {
-    id: 1,
-    description: 'Armo Sillas',
-    jobProvided: 'Sillas de roble',
-    category: JobCategoryModel.CARPINTERO,
-    price: 121,
-    totalRatings: 0,
-    averageRating: 0,
-    images: [],
-    provider: this.provider,
-    paused: false,
-    thumbnailImage: ""
-  };
-
-  // contactAux = new Contact(1,this.provider, this.provider, 'City', 'Street', 120, 1, 'C');
-  //
-  // contacts: Contact[] = [this.contactAux, this.contactAux, this.contactAux];
-
-  jpr: JobPaginationResult = {
-    results: [this.job, this.job, this.job, this.job, this.job],
-    page: 0,
-    totalPages: 4,
-  };
-
-  jpq: JobPaginationQuery = {
-    page: 0,
-    pageSize: 4,
-    order: OrderOptionModel.MOST_POPULAR
-  };
 
   activeTab: string = 'dashboard';
 
@@ -81,37 +28,18 @@ export class DashboardComponent implements OnInit, OnDestroy {
   user: User;
 
   constructor(
-    private jobsService: JobsService,
-    private userService: UserService
+    private userService: UserService,
   ) {
   }
 
   ngOnInit(): void {
-    this.jobsService.getJobs(this.jpq);
-    this.userSub = this.userService.user.subscribe(user => {
-      console.log(user)
+    this.userSub = this.userService.user.subscribe(user =>{
       this.user = user;
     })
   }
 
   changeTab(tab: string) {
     this.activeTab = tab;
-  }
-
-  getNumberOfWorks(): number {
-    return this.provider.providerDetails.jobsCount;
-  }
-
-  getAvgRating(): number {
-    return this.provider.providerDetails.avgRating;
-  }
-
-  getNumberOfReviews(): number {
-    return this.provider.providerDetails.reviewCount;
-  }
-
-  getNumberOfJobRequest(): number {
-    return 4;
   }
 
 
