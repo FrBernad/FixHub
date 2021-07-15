95031f3f1d60bf7ab9fb8145bb8bcde1c51336e2
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-07-14 21:31:41 -0300

    feat: language request filter

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/AcceptLanguageRequestFilter.java
@@ -0,0 +1,22 @@
+package ar.edu.itba.paw.webapp.config;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+@Provider
+public class AcceptLanguageRequestFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        final List<Locale> acceptableLanguages = requestContext.getAcceptableLanguages();
+
+        if(!acceptableLanguages.isEmpty())
+            LocaleContextHolder.setLocale(acceptableLanguages.get(0));
+    }
+}
