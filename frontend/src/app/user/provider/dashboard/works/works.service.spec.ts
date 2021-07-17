6d6ad5a8ec869ce0f6ecf4a23e14fb2d42ff0ee0
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-07-17 20:36:31 -0300

    feat: add works service

---
+++ frontend/src/app/user/provider/dashboard/works/works.service.spec.ts
@@ -0,0 +1,36 @@
+import {getTestBed, TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {WorksService} from "./works.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {DiscoverService} from "../../../../discover/discover.service";
+import {NotificationsService} from "../../../notifications/notifications.service";
+import {environment} from "../../../../../environments/environment";
+import {HttpStatusCode} from "@angular/common/http";
+
+describe('WorksService', () => {
+  let injector: TestBed;
+  let service: WorksService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WorksService],
+    });
+
+    injector = getTestBed();
+    service = injector.inject(WorksService);
+    httpMock = injector.inject(HttpTestingController);
+  });
+
+  it('getUserJobs(jp: JobPaginationQuery) should return user jobs', () => {
+    service.getUserJobs({page:0});
+
+    const req = httpMock.expectOne(environment.apiBaseUrl + '/user/jobs?page=0');
+    expect(req.request.method).toBe('GET');
+    req.flush({},{status: HttpStatusCode.Ok, statusText: HttpStatusCode.Ok.toString()});
+  });
+
+
+});
+
