d55bf2d31066f16a602eff47bbce80010afea187
Ignacio Agustin Manfredi <imanfredi@itba.edu.ar>
2021-07-17 23:22:50 -0300

    fix: front test

--- frontend/src/app/user/notifications/new-follower-notification-card/new-follower-notification-card.component.spec.ts
+++ frontend/src/app/user/notifications/new-follower-notification-card/new-follower-notification-card.component.spec.ts
@@ -2,14 +2,18 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NewFollowerNotificationCard} from './new-follower-notification-card.component';
 import {TestingModule} from "../../../testing.module";
+import {NotificationType} from "../../../models/notification-type-enum.model";
+import {LoadingSpinnerComponent} from "../../../shared/loading-spinner/loading-spinner.component";
 
 describe('NotificationCardComponent', () => {
   let component: NewFollowerNotificationCard;
   let fixture: ComponentFixture<NewFollowerNotificationCard>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NewFollowerNotificationCard ],
+      declarations: [ NewFollowerNotificationCard,
+                      LoadingSpinnerComponent],
       imports:[
         TestingModule
       ]
@@ -18,8 +22,14 @@ describe('NotificationCardComponent', () => {
   });
 
   beforeEach(() => {
+    const mockNotification = jasmine.createSpyObj(
+      'notification',
+      [],
+      {id:1,date:Date.now(),resource:2,seen:false,type:NotificationType.NEW_FOLLOWER}
+    )
     fixture = TestBed.createComponent(NewFollowerNotificationCard);
     component = fixture.componentInstance;
+    component.notification = mockNotification;
     fixture.detectChanges();
   });
 
--- frontend/src/app/user/requests/request-card/request-card.component.spec.ts
+++ frontend/src/app/user/requests/request-card/request-card.component.spec.ts
@@ -3,12 +3,13 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {RequestCardComponent} from './request-card.component';
 import {JobRequest} from "../../../models/job-request.model";
 import {TestingModule} from "../../../testing.module";
+import {ProviderDetails, User} from "../../../models/user.model";
+import {ContactInfo} from "../../../models/contact-info.model";
+import {JobStatus} from "../../../models/job-status-enum.model";
 
 describe('RequestCardComponent', () => {
   let component: RequestCardComponent;
   let fixture: ComponentFixture<RequestCardComponent>;
-  let mockType = 'Type';
-  let mockJobRequest = new JobRequest();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,10 +22,38 @@ describe('RequestCardComponent', () => {
   });
 
   beforeEach(() => {
+    const mockContactInfo = new ContactInfo(1, '', '', '', '');
+    const mockProviderDetails: ProviderDetails = {
+      location: {
+        cities: [{id: 1, name: ''}],
+        state: {id: 1, name: ''}
+      },
+      schedule: {
+        startTime: new Date(),
+        endTime: new Date()
+      },
+      jobsCount: 1, avgRating: 2, reviewCount: 3, contactsCount: 4
+    };
+    const mockUser = new User(1, 'name', 'surname', 'email', 'phoneNumber', 'state', 'city', 'profileImage', 'converImage', ['', ''], 1, 2, [mockContactInfo], undefined, true, false);
+    const mockProvider = new User(1, 'name', 'surname', 'email', 'phoneNumber', 'state', 'city', 'profileImage', 'converImage', ['', ''], 1, 2, [], mockProviderDetails, true, false);
+
+    const mockType = 'Type';
+    const mockJobRequest = {
+      id: 1,
+      jobProvided: '',
+      jobId: 2,
+      message: '',
+      status: JobStatus.IN_PROGRESS,
+      provider: mockProvider,
+      user: mockUser,
+      date: new Date(),
+      contactInfo: mockContactInfo
+    };
+
     fixture = TestBed.createComponent(RequestCardComponent);
     component = fixture.componentInstance;
     component.type=mockType;
-    component.request=mockJobRequest;
+    component.request = mockJobRequest;
     fixture.detectChanges();
   });
 
