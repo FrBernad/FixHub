94a161daf88a836aa0de741daa1fa3206be4f20d
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-07-19 17:10:58 -0300

    fix: conffig

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -45,12 +45,12 @@ public class WebConfig {
         final SimpleDriverDataSource ds = new SimpleDriverDataSource();
 
         ds.setDriverClass(org.postgresql.Driver.class);
-        ds.setUrl("jdbc:postgresql://localhost/paw");
-        ds.setUsername("root");
-        ds.setPassword("root");
-//        ds.setUrl("jdbc:postgresql://10.16.1.110/paw-2021a-06");
-//        ds.setUsername("paw-2021a-06");
-//        ds.setPassword("QroE40tsz");
+//        ds.setUrl("jdbc:postgresql://localhost/paw");
+//        ds.setUsername("root");
+//        ds.setPassword("root");
+        ds.setUrl("jdbc:postgresql://10.16.1.110/paw-2021a-06");
+        ds.setUsername("paw-2021a-06");
+        ds.setPassword("QroE40tsz");
         return ds;
     }
 
