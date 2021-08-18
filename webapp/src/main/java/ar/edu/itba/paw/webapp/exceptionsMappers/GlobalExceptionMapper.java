ea2770e8fec5ae2e9744b1451fb5e7925e7eee74
Fran <frbernad@itba.edu.ar>
2021-08-18 09:39:37 -0300

    fix: global mapper

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptionsMappers/GlobalExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptionsMappers/GlobalExceptionMapper.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -22,9 +23,18 @@ public class GlobalExceptionMapper extends Throwable implements ExceptionMapper<
     public Response toResponse(Throwable exception) {
         LOGGER.error("Error encountered, server error exception");
 
-        final String message = messageSource.getMessage("exception.ServerInternalError", null, LocaleContextHolder.getLocale());
+        final int status;
+        final String message;
 
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message).build();
+        if (exception instanceof WebApplicationException) {
+            status = ((WebApplicationException) exception).getResponse().getStatus();
+            message = exception.getMessage();
+        } else {
+            status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            message = messageSource.getMessage("exception.ServerInternalError", null, LocaleContextHolder.getLocale());
+        }
+
+        return Response.status(status).entity(message).build();
     }
 
 }
