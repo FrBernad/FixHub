aba91d24ff9c2240c787137f179c038180a1874f
Fran <frbernad@itba.edu.ar>
2021-10-11 20:31:00 -0300

    fix: i18n cache-busting

--- frontend/src/app/app-translate.module.ts
+++ frontend/src/app/app-translate.module.ts
@@ -1,24 +1,17 @@
 import {NgModule} from "@angular/core";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {HttpClient} from "@angular/common/http";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {environment} from "../environments/environment";
+import {WebpackTranslateLoader} from "./webpack-translate-loader";
 
 @NgModule({
   imports: [TranslateModule.forRoot({
     defaultLanguage: 'en',
     loader: {
-      provide: TranslateLoader,
-      useFactory: translateFactory,
-      deps: [HttpClient]
+        provide: TranslateLoader,
+        useClass: WebpackTranslateLoader
     }
   })],
   exports: [TranslateModule],
 })
 
 export class AppTranslateModule {
-}
-
-export function translateFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, environment.i18nBase);
 }
