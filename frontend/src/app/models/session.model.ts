9715742cc455a6824a02df00ab310376acf6e241
Fran <frbernad@itba.edu.ar>
2021-10-11 23:01:16 -0300

    refactor: add refresh token to session

--- frontend/src/app/models/session.model.ts
+++ frontend/src/app/models/session.model.ts
@@ -1,12 +1,13 @@
 export class Session {
   constructor(
     private _token: string,
-    private _tokenExpirationDate: Date
+    private _tokenExpirationDate: Date,
+    private _refreshToken: String,
   ) {}
 
   get token() {
     if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
-      return null;
+      return this._refreshToken;
     }
     return this._token;
   }
