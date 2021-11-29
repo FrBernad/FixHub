1e4c6be1e992507c22657603206dac0f1a718b3e
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-11-29 16:18:23 -0300

    fix: request component test

--- frontend/src/app/user/requests/request-detail/request.component.spec.ts
+++ frontend/src/app/user/requests/request-detail/request.component.spec.ts
@@ -1,11 +1,14 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 
 import {RequestComponent} from './request-detail.component';
 import {TestingModule} from "../../../testing.module";
+import {UserService} from "../../../auth/services/user.service";
 
 describe('RequestComponent', () => {
   let component: RequestComponent;
   let fixture: ComponentFixture<RequestComponent>;
+  let userService: UserService;
+  let injector: TestBed;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,8 +21,14 @@ describe('RequestComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ UserService ]
+    })
+    injector = getTestBed();
+    userService = injector.inject(UserService);
     fixture = TestBed.createComponent(RequestComponent);
     component = fixture.componentInstance;
+    userService.user.next({...userService.user.getValue(), roles: ['VERIFIED'], id: 1});
     fixture.detectChanges();
   });
 
