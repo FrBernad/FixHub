70b8a72637303858de918775a515bec07ae8ecfc
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-11-29 19:16:56 -0300

    fix: notification card component test

--- frontend/src/app/user/notifications/new-job-request-notification-card/new-job-request-notification-card.component.spec.ts
+++ frontend/src/app/user/notifications/new-job-request-notification-card/new-job-request-notification-card.component.spec.ts
@@ -1,13 +1,16 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 
 import {NewJobRequestNotificationCardComponent} from './new-job-request-notification-card.component';
 import {TestingModule} from "../../../testing.module";
 import {NotificationType} from "../../../models/notification-type-enum.model";
 import {Notification} from "../../../models/notification.model";
+import {UserService} from "../../../auth/services/user.service";
 
 describe('NewJobRequestNotificationCardComponent', () => {
   let component: NewJobRequestNotificationCardComponent;
   let fixture: ComponentFixture<NewJobRequestNotificationCardComponent>;
+  let injector: TestBed;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,11 +21,20 @@ describe('NewJobRequestNotificationCardComponent', () => {
   });
 
   beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      providers: [UserService]
+    });
+
+    injector = getTestBed();
+    userService = injector.inject(UserService);
+
     const mockNotification = new Notification(2, new Date(), 1, false, NotificationType.REQUEST_STATUS_CHANGE_USER_ACCEPTED);
 
     fixture = TestBed.createComponent(NewJobRequestNotificationCardComponent);
     component = fixture.componentInstance;
     component.notification = mockNotification;
+    userService.user.next({...userService.user.getValue(), roles: ['VERIFIED'], id: 1});
     fixture.detectChanges();
   });
 
