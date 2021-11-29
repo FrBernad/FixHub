966c01625b6aa144e81057e85b398f29cb0bb737
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-11-29 15:44:54 -0300

    fix: works test

--- frontend/src/app/user/provider/dashboard/works/works.component.spec.ts
+++ frontend/src/app/user/provider/dashboard/works/works.component.spec.ts
@@ -1,9 +1,12 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 
 import {WorksComponent} from './works.component';
 import {TestingModule} from "../../../../testing.module";
+import {UserService} from "../../../../auth/services/user.service";
 
 describe('WorksComponent', () => {
+  let injector: TestBed;
+  let userService: UserService;
   let component: WorksComponent;
   let fixture: ComponentFixture<WorksComponent>;
 
@@ -17,8 +20,15 @@ describe('WorksComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UserService]
+    });
+
+    injector = getTestBed();
+    userService = injector.inject(UserService);
     fixture = TestBed.createComponent(WorksComponent);
     component = fixture.componentInstance;
+    userService.user.next({...userService.user.getValue(), roles: ['VERIFIED'], id: 1});
     fixture.detectChanges();
   });
 
--- frontend/src/app/user/provider/dashboard/works/works.service.spec.ts
+++ frontend/src/app/user/provider/dashboard/works/works.service.spec.ts
@@ -3,27 +3,33 @@ import {HttpClientTestingModule, HttpTestingController} from "@angular/common/ht
 import {WorksService} from "./works.service";
 import {environment} from "../../../../../environments/environment";
 import {HttpStatusCode} from "@angular/common/http";
+import {RouterTestingModule} from "@angular/router/testing";
+import {UserService} from "../../../../auth/services/user.service";
 
 describe('WorksService', () => {
   let injector: TestBed;
   let service: WorksService;
+  let userService: UserService;
   let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [WorksService],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [WorksService, UserService],
     });
 
     injector = getTestBed();
+    userService = injector.inject(UserService);
     service = injector.inject(WorksService);
     httpMock = injector.inject(HttpTestingController);
+
+    userService.user.next({...userService.user.getValue(), roles: ['VERIFIED'], id: 1});
   });
 
   it('getUserJobs(jp: JobPaginationQuery) should return user jobs', () => {
     service.getUserJobs({page:0});
 
-    const req = httpMock.expectOne(environment.apiBaseUrl + '/user/jobs?page=0');
+    const req = httpMock.expectOne(environment.apiBaseUrl + '/users/' + userService.user.getValue().id + '/jobs?page=0');
     expect(req.request.method).toBe('GET');
     req.flush({},{status: HttpStatusCode.Ok, statusText: HttpStatusCode.Ok.toString()});
   });
